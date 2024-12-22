81ccc19a1b436d8112631ed425b6b972cd9c3b88
Brag72 <49995005+Brag72@users.noreply.github.com>
2024-12-22 21:43:24 +0300

    fix

--- QT_HW_Lesson3/mainwindow.cpp
+++ QT_HW_Lesson3/mainwindow.cpp
@@ -1,9 +1,10 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
+#include "dialog.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
@@ -13,3 +14,8 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_pushButton_clicked()
+{
+    Dialog* d(new Dialog);
+    d->show();
+}
